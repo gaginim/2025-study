b84187268aea2caed87983c20b6a781365e6c9b8
wanchub429 <wanchub429@gmail.com>
2025-01-29 01:48:26 +0900

    일부수정

--- spring-common-study/src/main/java/com/tommy/study/domain/myjob/controller/TestController.java
+++ spring-common-study/src/main/java/com/tommy/study/domain/myjob/controller/TestController.java
@@ -1,26 +1,48 @@
 package com.tommy.study.domain.myjob.controller;
 
 import com.tommy.study.domain.myjob.entity.MyJobTest;
+import com.tommy.study.domain.myjob.entity.MyJobTestRecord;
+import com.tommy.study.domain.myjob.repository.MyJobTestRecordRepository;
 import com.tommy.study.domain.myjob.repository.MyJobTestRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/test")
 @RequiredArgsConstructor
 @Tag(name = "Test API", description = "test init api..")
+@Log4j2
 public class TestController {
 
   private final MyJobTestRepository myJobTestRepository;
+  private final MyJobTestRecordRepository myJobTestRecordRepository;
 
   @Operation(summary = "Get MyJob", description = "test endpoint")
   @GetMapping("/{id}")
   public MyJobTest findAllByMyJobTest(@PathVariable(value = "id") Long id) {
-    return myJobTestRepository.findById(id).orElse(null);
+    var myJobTest = myJobTestRepository.findById(1L).stream().findFirst().orElse(null);
+    log.info("MyJobTest=> {}", myJobTest.getName());
+    return myJobTest;
+  }
+
+  @Operation(summary = "Save MyJob", description = "test endpoint")
+  @PostMapping("/{name}")
+  public MyJobTestRecord saveMyJobTest(@PathVariable(value = "name") String name) {
+    var myJobTest =
+        myJobTestRecordRepository.save(
+            MyJobTestRecord.builder()
+                .name(name)
+                .createdAt(LocalDateTime.now())
+                .createdBy(name)
+                .build());
+    log.info("MyJobTestRecord => {}", myJobTest.name());
+    return myJobTest;
   }
 }
