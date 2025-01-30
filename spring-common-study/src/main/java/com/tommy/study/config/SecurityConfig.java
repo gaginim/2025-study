3e606b5d3d981341b5328152b38749c0495d89b2
wanchub429 <wanchub429@gmail.com>
2025-01-30 13:34:39 +0900

    security 추가

---
+++ spring-common-study/src/main/java/com/tommy/study/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.tommy.study.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    return http.csrf(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(
+            auth -> auth.requestMatchers("/test/**").permitAll().anyRequest().authenticated())
+        .headers(config -> config.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+        .build();
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+    UserDetails user = User.withUsername("user").password("password").roles("USER").build();
+    return new InMemoryUserDetailsManager(user);
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+}
