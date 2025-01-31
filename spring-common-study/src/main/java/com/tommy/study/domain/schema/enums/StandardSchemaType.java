faee3c7616ebe57ba3c0d61b2ca8f67777326eae
wanchub429 <wanchub429@gmail.com>
2025-01-31 21:11:51 +0900

    standardSchemaType of method 추가

--- spring-common-study/src/main/java/com/tommy/study/domain/schema/enums/StandardSchemaType.java
+++ spring-common-study/src/main/java/com/tommy/study/domain/schema/enums/StandardSchemaType.java
@@ -1,7 +1,18 @@
 package com.tommy.study.domain.schema.enums;
 
+import com.tommy.study.exception.ApplicationException;
+import com.tommy.study.exception.ApplicationExceptionCode;
+import java.util.Arrays;
+
 public enum StandardSchemaType {
   EVENT,
   INTERVAL,
-  STOPWATCH
+  STOPWATCH;
+
+  public static StandardSchemaType of(String name) {
+    return Arrays.stream(StandardSchemaType.values())
+        .filter(s -> name.equals(s.name()))
+        .findFirst()
+        .orElseThrow(() -> ApplicationException.of(ApplicationExceptionCode.BAD_REQUEST));
+  }
 }
