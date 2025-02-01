f3c885d88d0e262a295e54806ca0e37c7f895b79
wanchub429 <wanchub429@gmail.com>
2025-02-01 14:58:57 +0900

    add jpa module

---
+++ spring-jpa-study/src/main/java/com/tommy/study/SpringJpaStudyApplication.java
@@ -0,0 +1,12 @@
+package com.tommy.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaStudyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringJpaStudyApplication.class, args);
+  }
+}
---
+++ spring-jpa-study/src/test/java/com/tommy/study/SpringJpaStudyApplicationTests.java
@@ -0,0 +1,11 @@
+package com.tommy.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaStudyApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
