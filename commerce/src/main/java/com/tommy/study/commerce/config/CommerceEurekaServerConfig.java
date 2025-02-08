607ed2336b1eb625742067e038061e8eed0263c8
wanchub429 <wanchub429@gmail.com>
2025-02-09 02:12:36 +0900

    불필요한 모듈 삭제

--- commerce/src/main/java/com/tommy/study/commerce/config/CommerceEurekaServerConfig.java
+++
@@ -1,8 +0,0 @@
-package com.tommy.study.commerce.config;
-
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableEurekaServer
-public class CommerceEurekaServerConfig {}
