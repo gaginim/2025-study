9e631adcf227232810cca40a09e8e9e34bc41044
wanchub429 <wanchub429@gmail.com>
2025-02-09 01:25:05 +0900

    eureka discovery user service 추가

---
+++ discovery-user-service/src/main/java/com/tommy/study/discoveryuserservice/DiscoveryUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tommy.study.discoveryuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryUserServiceApplication.class, args);
+	}
+
+}
---
+++ discovery-user-service/src/test/java/com/tommy/study/discoveryuserservice/DiscoveryUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tommy.study.discoveryuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
