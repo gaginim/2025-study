6f7ee88b3ae99dad9b4fd121bef2edecd65547ce
wanchub429 <wanchub429@gmail.com>
2025-02-09 02:06:24 +0900

    readme 일부 기입

--- discovery-user-service/README.md
+++ discovery-user-service/README.md
@@ -2,3 +2,9 @@
 
 - 유레카 서비스
 
+- 포트 달리해서 동일 서비스 띄우는 법
+  VM Option 추가: -Dserver.port=9003
+
+- command 로 다른 port 번호 줘서 실행하는법
+  ./gradlew :discovery-user-service:clean :discovery-user-service:build :discovery-user-service:bootRun
+  --args='--server.port=9004'
--- discovery-user-service/src/main/java/com/tommy/study/discoveryuserservice/DiscoveryUserServiceApplication.java
+++ discovery-user-service/src/main/java/com/tommy/study/discoveryuserservice/DiscoveryUserServiceApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DiscoveryUserServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DiscoveryUserServiceApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(DiscoveryUserServiceApplication.class, args);
+  }
 }
