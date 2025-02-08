3c263552845bc1f05b3528c8822a42675f21a8bb
wanchub429 <wanchub429@gmail.com>
2025-02-09 01:07:38 +0900

    eureka discovery service 추가

---
+++ discoveryservice/src/test/java/com/tommy/study/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.tommy.study.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
