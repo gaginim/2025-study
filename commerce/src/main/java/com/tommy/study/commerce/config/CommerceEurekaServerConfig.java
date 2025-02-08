97646ef2e05a4c066092001b90e7beb01dfaf92f
wanchub429 <wanchub429@gmail.com>
2025-02-09 00:57:24 +0900

    eureka server 생성

---
+++ commerce/src/main/java/com/tommy/study/commerce/config/CommerceEurekaServerConfig.java
@@ -0,0 +1,8 @@
+package com.tommy.study.commerce.config;
+
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableEurekaServer
+public class CommerceEurekaServerConfig {}
