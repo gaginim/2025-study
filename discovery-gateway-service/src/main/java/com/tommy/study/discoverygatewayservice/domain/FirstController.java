08af0fd755da02580d07c8829d7d9257782d5851
wanchub429 <wanchub429@gmail.com>
2025-02-09 16:43:10 +0900

    FilterConfig로 처리하기

--- discovery-gateway-service/src/main/java/com/tommy/study/discoverygatewayservice/domain/FirstController.java
+++ discovery-gateway-service/src/main/java/com/tommy/study/discoverygatewayservice/domain/FirstController.java
@@ -1,10 +1,13 @@
 package com.tommy.study.discoverygatewayservice.domain;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Log4j2
 @Profile("local")
 @RestController
 @RequestMapping("/first-service")
@@ -13,5 +16,11 @@ public class FirstController {
   @GetMapping("welcome")
   public String welcome() {
     return "Good luck first me.";
+  }
+
+  @GetMapping("message")
+  public String message(@RequestHeader(value = "first-request", required = false) String header) {
+    log.info("header: {}", header);
+    return "hello luck first me.";
   }
 }
--- discovery-gateway-service/src/main/java/com/tommy/study/discoverygatewayservice/domain/SecondController.java
+++ discovery-gateway-service/src/main/java/com/tommy/study/discoverygatewayservice/domain/SecondController.java
@@ -1,10 +1,13 @@
 package com.tommy.study.discoverygatewayservice.domain;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Log4j2
 @Profile("test")
 @RestController
 @RequestMapping("/second-service")
@@ -13,5 +16,11 @@ public class SecondController {
   @GetMapping("welcome")
   public String welcome() {
     return "Good luck second me.";
+  }
+
+  @GetMapping("message")
+  public String message(@RequestHeader(value = "second-request", required = false) String header) {
+    log.info("header: {}", header);
+    return "hello luck second me.";
   }
 }
