c2e9e234a9d3d2aa35659f21f8ce04dec37f18f3
wanchub429 <wanchub429@gmail.com>
2025-02-09 16:22:07 +0900

    중간수정

--- discovery-gateway-service/src/main/java/com/tommy/study/discoverygatewayservice/domain/FirstController.java
+++ discovery-gateway-service/src/main/java/com/tommy/study/discoverygatewayservice/domain/FirstController.java
@@ -1,15 +1,17 @@
 package com.tommy.study.discoverygatewayservice.domain;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Profile("local")
 @RestController
-@RequestMapping("/second-service")
+@RequestMapping("/first-service")
 public class FirstController {
 
   @GetMapping("welcome")
   public String welcome() {
-    return "Good luck second me.";
+    return "Good luck first me.";
   }
 }
--- discovery-gateway-service/src/main/java/com/tommy/study/discoverygatewayservice/domain/SecondController.java
+++ discovery-gateway-service/src/main/java/com/tommy/study/discoverygatewayservice/domain/SecondController.java
@@ -1,15 +1,17 @@
 package com.tommy.study.discoverygatewayservice.domain;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Profile("test")
 @RestController
-@RequestMapping("/first-service")
+@RequestMapping("/second-service")
 public class SecondController {
 
   @GetMapping("welcome")
   public String welcome() {
-    return "Good luck first me.";
+    return "Good luck second me.";
   }
 }
