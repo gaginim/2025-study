d7892a0f56b699ea98d4cb7e476833288e826fb6
wanchub429 <wanchub429@gmail.com>
2025-03-04 23:58:36 +0900

    redis 세션 실습

--- redis-sample/src/main/java/com/tommy/study/redissample/domain/LoginController.java
+++ redis-sample/src/main/java/com/tommy/study/redissample/domain/LoginController.java
@@ -1,23 +1,21 @@
 package com.tommy.study.redissample.domain;
 
 import jakarta.servlet.http.HttpSession;
-import java.util.HashMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginController {
-  private HashMap<String, String> sessionMap = new HashMap<>();
 
   @GetMapping("/login")
   public String login(HttpSession httpSession, @RequestParam String name) {
-    sessionMap.put(httpSession.getId(), name);
+    httpSession.setAttribute("name", name);
     return "saved " + name;
   }
 
   @GetMapping("/myName")
   public String getMyName(HttpSession httpSession, @RequestParam String name) {
-    return sessionMap.get(httpSession.getId());
+    return (String) httpSession.getAttribute("name");
   }
 }
