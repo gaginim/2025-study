56ba56c97359a2d58336e95252f2c9e565eb61ab
wanchub429 <wanchub429@gmail.com>
2025-03-10 22:44:29 +0900

    todo 로 남김

--- codeing-test/src/main/java/com/example/codeingtest/twopointer/Main1253.java
+++ codeing-test/src/main/java/com/example/codeingtest/twopointer/Main1253.java
@@ -1,6 +1,5 @@
 package com.example.codeingtest.twopointer;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main1253 {
@@ -15,36 +14,7 @@ public class Main1253 {
     for (int i = 0; i < size; i++) {
       map[i] = sc.nextInt();
     }
-
-    Arrays.sort(map);
-
-    int result = 0;
-    for (int i = 0; i < map.length; i++) {
-      int goal = map[i];
-      int left = 0;
-      int right = map.length - 1;
-
-      while (left < right) {
-        if (left == i) {
-          left++;
-          continue;
-        }
-        if (right == i) {
-          right--;
-          continue;
-        }
-
-        if (map[left] + map[right] == goal) {
-          result++;
-          break;
-        } else if (map[left] + map[right] > goal) {
-          right--;
-        } else {
-          left++;
-        }
-      }
-    }
-
-    System.out.println(result);
+    
+    // todo: 주말에 다시 풀기
   }
 }
--- codeing-test/src/main/java/com/example/codeingtest/twopointer/Main2470.java
+++ codeing-test/src/main/java/com/example/codeingtest/twopointer/Main2470.java
@@ -1,6 +1,5 @@
 package com.example.codeingtest.twopointer;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main2470 {
@@ -14,30 +13,7 @@ public class Main2470 {
     for (int i = 0; i < size; i++) {
       map[i] = sc.nextInt();
     }
-    Arrays.sort(map);
 
-    int min = Integer.MAX_VALUE;
-    int left = 0;
-    int right = map.length - 1;
-
-    int[] result = new int[2];
-    while (left < right) {
-      int sum = map[left] + map[right];
-      int sumAbs = Math.abs(sum);
-
-      if (sumAbs < min) {
-        min = sumAbs;
-        result[0] = map[left];
-        result[1] = map[right];
-      }
-
-      if (sum > 0) {
-        right--;
-      } else {
-        left++;
-      }
-    }
-
-    System.out.println(result[0] + " " + result[1]);
+    // todo: 주말에 다시 풀기
   }
 }
