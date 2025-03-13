802cddc9329b674e8a5572e1a53a16ef247255e7
wanchub429 <wanchub429@gmail.com>
2025-03-13 23:15:03 +0900

    1644 백준 풀기

---
+++ codeing-test/src/main/java/com/example/codeingtest/twopointer/Main1644.java
@@ -0,0 +1,44 @@
+package com.example.codeingtest.twopointer;
+
+import java.util.*;
+
+/*
+ 반드시 해야할 점: 소수점 만들기!!
+*/
+public class Main1644 {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    int num = sc.nextInt();
+    boolean[] numbers = new boolean[num + 1];
+    ArrayList<Integer> prime_numbers = new ArrayList<>();
+
+    numbers[0] = numbers[1] = true;
+    for (int i = 2; i * i <= num; i++) {
+      if (!numbers[i]) for (int j = i * i; j <= num; j += i) numbers[j] = true;
+    }
+    for (int i = 1; i <= num; i++) {
+      if (!numbers[i]) prime_numbers.add(i);
+    }
+
+    int result = 0;
+    int start = 0;
+    int end = 0;
+    int temp = 0;
+
+    while (true) {
+      if (temp > num) {
+        temp -= prime_numbers.get(start++);
+      } else if (temp < num) {
+        if (end == prime_numbers.size()) break;
+        temp += prime_numbers.get(end++);
+      } else {
+        result++;
+        temp -= prime_numbers.get(start++);
+      }
+    }
+
+    System.out.println(result);
+  }
+}
